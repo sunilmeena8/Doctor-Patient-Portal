b01e89a3b5ecdeb81330a7b60da9b689972f5569
sunil <sunil98meena@gmail.com>
2019-11-03 21:17:32 +0530

    models

--- dpp/portal/models.py
+++ dpp/portal/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+	id=models.IntegerField(primary_key=True)
+	name=models.CharField(max_length=20)
+	occupation=models.CharField(max_length=20)
+
+class Doctor(models.Model):
+	id=models.IntegerField(primary_key=True)
+	name=models.CharField(max_length=20)
+	
+class Patient(models.Model):
+	id=models.IntegerField(primary_key=True)
+	name=models.CharField(max_length=20)
--- dpp/portal/views.py
+++ dpp/portal/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login,logout,authenticate
 from .forms import CustomForm
+from .models import Person,Patient,Doctor
 # Create your views here.
 def homeview(request):
     return render(request , 'portal/home.html' , {})
@@ -14,9 +15,23 @@ def register(request):
 			form.save()
 			username=form.cleaned_data.get('username')
 			password=form.cleaned_data.get('password1')
+			occupation=form.cleaned_data.get('occupation')
+			person=Person()
+			person.name=username
+			person.occupation=occupation
+			person.save()
+			if(occupation=='doctor'):
+				doctor=Doctor()
+				doctor.name=username
+				doctor.save()
+
+			else:
+				patient=Patient()
+				patient.name=username
+				patient.save()
 			user=authenticate(username=username,password=password)
 			login(request,user)
-			return render(request,'portal/home.html')
+			return render(request,'portal/home.html',)
 		else:
 			pass
 	else:
